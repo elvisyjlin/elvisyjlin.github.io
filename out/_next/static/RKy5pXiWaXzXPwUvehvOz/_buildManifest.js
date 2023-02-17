90d1ad9d155eb7144c33fe0137b4f444c125b99c
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2023-02-18 01:56:21 +0800

    Build to docs/

--- out/_next/static/RKy5pXiWaXzXPwUvehvOz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e2b1cdff8bbcca91.css","static/chunks/pages/index-8f072e7c8c30b313.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
